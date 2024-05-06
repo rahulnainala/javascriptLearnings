7912c5d758436b97e554d087ca72aedaf1cf1b55
Rahul Nainala <rahulnainala2@gmail.com>
2024-05-06 20:52:46 +0530

    callback updated

--- CallBacks/index.js
+++ CallBacks/index.js
@@ -1,1 +1,15 @@
 //what is a callback function in javascript
+
+//Javascript is a synchronus and single-threaded language
+
+//Blocking the main thread
+
+//power of callbacks?
+
+//Deep about Event Listeners
+
+//Closures Demo with event Listeners
+
+//Scope Demo with Event Listeners
+
+//Garbage Collection & removeEventListners
